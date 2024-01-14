8cf774f35df5870fcc4cdc78c5f00707be715f18
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-01-15 00:55:17 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -33,9 +33,7 @@ st.set_page_config(
 #Author: Sergio Demis Lopez Martinez
 #------------------------------------------------------------
 #HEADER
-#st.markdown('''
-Powered by Tanishq <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--iU3HTvsb--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/d2utudhcrkw5ev8f8j0a.jpg">
-''', unsafe_allow_html=True)
+#st.markdown('''Powered by Tanishq <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--iU3HTvsb--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/d2utudhcrkw5ev8f8j0a.jpg">''', unsafe_allow_html=True)
 
 image = Image.open("DALL·E 2024-01-15 00.41.44 - A cartoon image of a software developer surrounded by various AI tools. The character is whimsical and colorful, with exaggerated features typical of .png")
 
