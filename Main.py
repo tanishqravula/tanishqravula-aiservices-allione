a9f462529855b3d2a044376d54b8104848f9a48b
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-01-23 21:53:17 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -171,24 +171,8 @@ def extract_text_from_website(url):
         tables = soup.find_all("table")
         table_text = "\n\n".join([pd.read_html(str(table))[0].to_string(index=False) for table in tables])
 
-        # Extract text from images using Tesseract OCR
-        images = soup.find_all("img")
-        image_text = ""
-        for idx, image in enumerate(images):
-            img_url = image['src']
-            img_response = requests.get(img_url, stream=True)
-            img_response.raise_for_status()  # Raise HTTPError for bad image requests
-            img_content_type = img_response.headers.get('content-type', '')
-            
-            # Check if the response contains image data
-            if 'image' in img_content_type:
-                img = Image.open(BytesIO(img_response.content))
-                img_text = pytesseract.image_to_string(img)
-                image_text += f"Image {idx + 1} Text:\n{img_text}\n\n"
-            else:
-                image_text += f"Image {idx + 1}: Not a valid image\n\n"
 
-        return f"{text_content}\n\nTable Text:\n{table_text}\n{image_text}"
+        return f"{text_content}\n\nTable Text:\n{table_text}\n"
     except Exception as e:
         return f"Error: {e}"
 
