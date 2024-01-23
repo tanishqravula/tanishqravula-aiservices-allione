a96555d68ee336478e3b3dcc97da76683e1a5f55
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-01-23 22:25:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -167,6 +167,7 @@ def extract_text_from_website(url):
         # Use Selenium to load dynamic content
         options = Options()
         options.add_argument('--headless')  # Run Chrome in headless mode
+        options.add_argument('--no-sandbox')  # Add no-sandbox option
         driver = webdriver.Chrome(options=options)
         driver.get(url)
         dynamic_content = driver.page_source
