2341759f18cd1a9cd77944038cfa5212349b1b7f
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-03-16 14:51:11 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -599,17 +599,7 @@ if prompt:
       spinertxt = 'Wait a moment, I am thinking...'
     with st.spinner(spinertxt):
         if len(prmt['parts']) > 1:
-            response = vision.generate_content(prmt['parts'],stream=True,safety_settings=[
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_LOW_AND_ABOVE",
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_LOW_AND_ABOVE",
-        },
-    ]
-)
+            response = vision.generate_content(prmt['parts'],stream=True)
             response.resolve()
         else:
             response = st.session_state.chat.send_message(prmt['parts'][0])
