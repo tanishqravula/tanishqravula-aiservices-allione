49e2f4e70d2858863f61d92d564f52691a5b6c2e
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2024-06-13 09:28:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import streamlit as st
+qimport streamlit as st
 import pandas as pd
 import google.generativeai as genai
 import re
@@ -116,7 +116,7 @@ def load_modelvision() -> genai.GenerativeModel:
     The function load_modelvision loads a generative model for vision tasks using the gemini-pro-vision model.
     :return: an instance of the genai.GenerativeModel class.
     """
-    model = genai.GenerativeModel('gemini-pro-vision')
+    model = genai.GenerativeModel(name='gemini-1.5-flash’)
     return model
 def to_markdown(text):
     text = text.replace('•', '  *')
